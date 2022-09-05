b6abe643f1e2419a37235a6d6b23dd53736c3ab4
Fortyseven <fortyseven@network47.org>
2022-09-05 09:55:11 -0400

    Enormous updates; includes img2img integration

--- server/server.py
+++ server/server.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
+import io
 import mimetypes
 import numpy as np
 import os
 import re
 import time
 import torch
+import urllib.parse
 
 from torch import autocast
 from pytorch_lightning import seed_everything
 from contextlib import nullcontext
-from einops import rearrange
+from einops import rearrange, repeat
 from omegaconf import OmegaConf
 from PIL import Image
 from random import randint
@@ -19,10 +21,16 @@ from sd.ldm.util import instantiate_from_config
 from sd.optimUtils import split_weighted_subprompts
 
 from flask import Flask, request, make_response
+from flask_cors import CORS
 
 
 # FIXME
 TMP_IMAGE_CACHE_PATH = "/tmp/sdfart"
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = TMP_IMAGE_CACHE_PATH
+CORS(app)
 
 
 def load_model_from_config(ckpt, verbose=False):
@@ -32,6 +40,24 @@ def load_model_from_config(ckpt, verbose=False):
         print(f"Global Step: {pl_sd['global_step']}")
     sd = pl_sd["state_dict"]
     return sd
+
+
+def load_img(image, h0, w0):
+    image = image.convert("RGB")
+    w, h = image.size
+    print(f"loaded input image of size ({w}, {h})")
+    if h0 is not None and w0 is not None:
+        h, w = h0, w0
+
+    # resize to integer multiple of 32
+    w, h = map(lambda x: x - x % 64, (w, h))
+
+    print(f"New image size ({w}, {h})")
+    image = image.resize((w, h), resample=Image.LANCZOS)
+    image = np.array(image).astype(np.float32) / 255.0
+    image = image[None].transpose(0, 3, 1, 2)
+    image = torch.from_numpy(image)
+    return 2.0 * image - 1.0
 
 
 def init():
@@ -90,7 +116,7 @@ def save_images(seed, prompt, ddim_steps, ddim_eta, sampler, scale, width, heigh
     base_count = len(os.listdir(sample_path))
 
     # seeds = []
-    paths = dict()
+    paths = []
 
     for i in range(batch_size):
         x_samples_ddim = modelFS.decode_first_stage(
@@ -117,15 +143,16 @@ def save_images(seed, prompt, ddim_steps, ddim_eta, sampler, scale, width, heigh
 
         Image.fromarray(x_sample.astype(np.uint8)).save(save_name)
 
-        paths[str(seed)] = {
+        paths.append({
             "path": save_name,
+            "seed": str(seed),
             "ddim_steps": ddim_steps,
             "ddim_eta": ddim_eta,
             "sampler": sampler,
             "scale": scale,
             "width": width,
             "height": height,
-        }
+        })
 
         seed += 1
         base_count += 1
@@ -134,6 +161,166 @@ def save_images(seed, prompt, ddim_steps, ddim_eta, sampler, scale, width, heigh
         del x_samples_ddim
 
     return paths
+
+
+def generate_img2img(
+    prompt,
+    ddim_steps,
+    batch_size,
+    height,
+    width,
+    scale,
+    ddim_eta,
+    unet_bs,
+    device,
+    seed,
+    turbo,
+    full_precision,
+    strength,
+    image
+):
+
+    print("############################################generate_img2img")
+    if seed == "":
+        seed = randint(0, 1000000)
+
+    seed = int(seed)
+    seed_everything(seed)
+
+    # Logging
+    sampler = "ddim"
+
+    init_image = load_img(image, height, width).to(device)
+    model.unet_bs = unet_bs
+    model.turbo = turbo
+    model.cdevice = device
+    modelCS.cond_stage_model.device = device
+
+    if device != "cpu" and full_precision == False:
+        model.half()
+        modelCS.half()
+        modelFS.half()
+        init_image = init_image.half()
+
+    tic = time.time()
+    # n_rows = opt.n_rows if opt.n_rows > 0 else batch_size
+    assert prompt is not None
+    data = [batch_size * [prompt]]
+
+    modelFS.to(device)
+
+    init_image = repeat(init_image, "1 ... -> b ...", b=batch_size)
+    init_latent = modelFS.get_first_stage_encoding(
+        modelFS.encode_first_stage(init_image))  # move to latent space
+
+    if device != "cpu":
+        mem = torch.cuda.memory_allocated() / 1e6
+        modelFS.to("cpu")
+        while torch.cuda.memory_allocated() / 1e6 >= mem:
+            time.sleep(1)
+
+    assert 0.0 <= strength <= 1.0, "can only work with strength in [0.0, 1.0]"
+    t_enc = int(strength * ddim_steps)
+    print(f"target t_enc is {t_enc} steps")
+
+    if full_precision == False and device != "cpu":
+        precision_scope = autocast
+    else:
+        precision_scope = nullcontext
+
+    all_samples = []
+    seeds = ""
+    with torch.no_grad():
+        all_samples = list()
+        for prompts in data:
+            with precision_scope("cuda"):
+                modelCS.to(device)
+
+                uc = None
+
+                if scale != 1.0:
+                    uc = modelCS.get_learned_conditioning(
+                        batch_size * [""])
+
+                if isinstance(prompts, tuple):
+                    prompts = list(prompts)
+
+                subprompts, weights = split_weighted_subprompts(prompts[0])
+
+                if len(subprompts) > 1:
+                    c = torch.zeros_like(uc)
+                    totalWeight = sum(weights)
+                    # normalize each "sub prompt" and add it
+                    for i in range(len(subprompts)):
+                        weight = weights[i]
+                        # if not skip_normalize:
+                        weight = weight / totalWeight
+                        c = torch.add(c, modelCS.get_learned_conditioning(
+                            subprompts[i]), alpha=weight)
+                else:
+                    c = modelCS.get_learned_conditioning(prompts)
+
+                if device != "cpu":
+                    mem = torch.cuda.memory_allocated() / 1e6
+                    modelCS.to("cpu")
+                    while torch.cuda.memory_allocated() / 1e6 >= mem:
+                        time.sleep(1)
+
+                # encode (scaled latent)
+                z_enc = model.stochastic_encode(
+                    init_latent, torch.tensor(
+                        [t_enc] * batch_size).to(device),
+                    seed,
+                    ddim_eta,
+                    ddim_steps
+                )
+                # decode it
+                samples_ddim = model.sample(
+                    t_enc,
+                    c,
+                    z_enc,
+                    unconditional_guidance_scale=scale,
+                    unconditional_conditioning=uc,
+                    sampler=sampler
+                )
+
+                modelFS.to(device)
+                print("saving images")
+
+                results = save_images(
+                    seed,
+                    prompt,
+                    ddim_steps,
+                    ddim_eta,
+                    sampler,
+                    scale,
+                    width,
+                    height,
+                    batch_size,
+                    samples_ddim,
+                    all_samples)
+
+                if device != "cpu":
+                    mem = torch.cuda.memory_allocated() / 1e6
+                    modelFS.to("cpu")
+                    while torch.cuda.memory_allocated() / 1e6 >= mem:
+                        time.sleep(1)
+
+                del samples_ddim
+
+                # print("memory_final = ", torch.cuda.memory_allocated() / 1e6)
+
+    toc = time.time()
+    time_taken = (toc - tic) / 60.0
+
+    # grid = torch.cat(all_samples, 0)
+    # grid = make_grid(grid, nrow=n_iter)
+    # grid = 255.0 * rearrange(grid, "c h w -> h w c").cpu().numpy()
+
+    mins = str(round(time_taken, 3))
+    txt = (f"Finished in {mins} minutes.")
+
+    return results
 
 
 def generate(
@@ -151,10 +338,12 @@ def generate(
     full_precision,
     sampler,
 ):
-    print("############################################")
+    print("############################################generate")
     C = 4
     f = 8
+
     start_code = None
+
     model.unet_bs = unet_bs
     model.turbo = turbo
     model.cdevice = device
@@ -193,6 +382,7 @@ def generate(
                 modelCS.to(device)
 
                 uc = None
+
                 if scale != 1.0:
                     uc = modelCS.get_learned_conditioning(
                         batch_size * [""])
@@ -261,60 +451,70 @@ def generate(
 
                 del samples_ddim
 
-                print("memory_final = ", torch.cuda.memory_allocated() / 1e6)
-
     toc = time.time()
     time_taken = (toc - tic) / 60.0
-
-    # grid = torch.cat(all_samples, 0)
-    # grid = make_grid(grid, nrow=n_iter)
-    # grid = 255.0 * rearrange(grid, "c h w -> h w c").cpu().numpy()
 
     mins = str(round(time_taken, 3))
     txt = (f"Finished in {mins} minutes.")
 
     return results
-    # returrestulte.fromarray(grid.astype(np.uint8)), txt
-
-
-app = Flask(__name__)
-
-SD_ROOT_SCRIPTS = "./sd/scripts/"
 
 
 @app.get("/get_image")
 def api_get_image():
     # FIXME: sanitize this path; this is DANGEROUS as-is
-    file_path = request.query_string
+    file_path = urllib.parse.unquote(request.query_string)
     with open(file_path, "rb") as f:
         response = make_response(f.read())
-        response.headers.set('Content-Type', 'image/jpeg')
+        response.headers.set('Content-Type', 'image/png')
         return response
 
 
 @app.post("/generate")
 def api_generate():
-    settings = request.get_json()
+    image = None
+    result_paths = []
 
-    result_paths = generate(
-        prompt=settings.get("prompt", ""),
-        ddim_steps=settings.get("ddim_steps", 50),
-        # n_iter=settings.get("n_iter", 1),
-        batch_size=settings.get("batch_size", 1),
-        height=settings.get("height", ""),
-        width=settings.get("width", ""),
-        scale=settings.get("scale", ""),
-        ddim_eta=settings.get("ddim_eta", ""),
-        unet_bs=settings.get("unet_bs", 1),
-        device=settings.get("device", "cuda"),
-        seed=settings.get("seed", 0),
-        turbo=settings.get("turbo", True),
-        full_precision=settings.get("full_precision", False),
-        sampler=settings.get("sampler", "plms")
-    )
+    try:
+        if request.files:
+            image_data = request.files['file'].read()
+            image = Image.open(io.BytesIO(image_data))
+            result_paths = generate_img2img(
+                prompt=request.form.get("prompt", ""),
+                ddim_steps=int(request.form.get("ddim_steps", 50)),
+                batch_size=int(request.form.get("batch_size", 1)),
+                height=int(request.form.get("height", "")),
+                width=int(request.form.get("width", "")),
+                scale=float(request.form.get("scale", 7.5)),
+                ddim_eta=float(request.form.get("ddim_eta", "")),
+                unet_bs=int(request.form.get("unet_bs", 1)),
+                device=request.form.get("device", "cuda"),
+                seed=request.form.get("seed", ''),
+                turbo=bool(request.form.get("turbo", True)),
+                full_precision=bool(request.form.get("full_precision", False)),
+                strength=float(request.form.get("strength", 0.5)),
+                image=image
+            )
+        else:
+            result_paths = generate(
+                prompt=request.form.get("prompt", ""),
+                ddim_steps=int(request.form.get("ddim_steps", 50)),
+                batch_size=int(request.form.get("batch_size", 1)),
+                height=int(request.form.get("height", "")),
+                width=int(request.form.get("width", "")),
+                scale=float(request.form.get("scale", 7.5)),
+                ddim_eta=float(request.form.get("ddim_eta", "")),
+                unet_bs=int(request.form.get("unet_bs", 1)),
+                device=request.form.get("device", "cuda"),
+                seed=request.form.get("seed", ''),
+                turbo=bool(request.form.get("turbo", True)),
+                full_precision=bool(request.form.get("full_precision", False)),
+                sampler=request.form.get("sampler", "plms"),
+            )
+    except:
+        print("EXCEPTION!")
+        torch.cuda.empty_cache()
 
-    # dir(request)
-    # return f"<h1>Ok...?</h1><plaintext>{status}"
     return result_paths
 
 
