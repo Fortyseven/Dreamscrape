4a1123c5b693a18f24b74e8178d07b82c0285bd6
Fortyseven <fortyseven@network47.org>
2022-09-16 14:58:43 -0400

    Enter to generate now works on seed field

--- client/src/main/prompt/Prompt.svelte
+++ client/src/main/prompt/Prompt.svelte
@@ -1,33 +1,37 @@
 <script>
-  import { onMount, createEventDispatcher } from "svelte";
+    import { onMount, createEventDispatcher } from "svelte";
 
-  import { FormGroup, Input } from "sveltestrap";
-  import { seed, prompt, is_loading } from "../store";
-  import WaitingBar from "../ui/WaitingBar.svelte";
+    import { FormGroup, Input } from "sveltestrap";
+    import { seed, prompt, is_loading } from "../store";
+    import WaitingBar from "../ui/WaitingBar.svelte";
 
-  const dispatch = createEventDispatcher();
-  let inner;
+    const dispatch = createEventDispatcher();
+    let inner;
 
-  onMount(async () => {
-    inner.focus();
-  });
+    onMount(async () => {
+        inner.focus();
+    });
 
-  function onPromptKeydown(e) {
-    if (e.keyCode === 13) {
-      dispatch("generate");
+    function onPromptKeydown(e) {
+        if (e.keyCode === 13) {
+            dispatch("generate");
+        }
     }
-  }
 </script>
 
 <FormGroup floating label="Prompt">
-  <Input
-    bind:value={$prompt}
-    disabled={$is_loading}
-    on:keydown={onPromptKeydown}
-    bind:inner
-  />
+    <Input
+        bind:value={$prompt}
+        disabled={$is_loading}
+        on:keydown={onPromptKeydown}
+        bind:inner
+    />
 </FormGroup>
 
 <FormGroup floating label="Seed">
-  <Input bind:value={$seed} disabled={$is_loading} />
+    <Input
+        bind:value={$seed}
+        disabled={$is_loading}
+        on:keydown={onPromptKeydown}
+    />
 </FormGroup>
