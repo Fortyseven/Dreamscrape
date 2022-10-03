dc8bf419184dfcbab74cebe01b2181ba4ad08150
Fortyseven <fortyseven@network47.org>
2022-10-03 19:42:40 -0400

    Formatting + fixes persistent result image

--- client/src/main/results/ResultPanel.svelte
+++ client/src/main/results/ResultPanel.svelte
@@ -1,24 +1,24 @@
 <script>
-  import { gen_results, result_selected } from "../store";
-  import ControlBar from "./ControlBar.svelte";
-  import PreviewThumbs from "./PreviewThumbs.svelte";
+    import { gen_results, result_selected } from "../store";
+    import ControlBar from "./ControlBar.svelte";
+    import PreviewThumbs from "./PreviewThumbs.svelte";
 
-  let result_image = null;
+    let result_image = null;
 
-  /* ----------------------------------------------*/
-  $: if (result_image && $gen_results.length > 0) {
-    result_image.src =
-      "data:image/jpeg;base64," + $gen_results[$result_selected].image;
-  }
+    /* ----------------------------------------------*/
+    $: if (result_image && $gen_results.length > 0) {
+        result_image.src =
+            "data:image/jpeg;base64," + $gen_results[$result_selected].image;
+    }
 </script>
 
 <div>
-  {#if $gen_results.length > 0}
-    <PreviewThumbs />
-    <div style="width: {$gen_results[$result_selected].width}px">
-      <ControlBar />
-    </div>
-  {/if}
-  <!-- svelte-ignore a11y-missing-attribute -->
-  <img bind:this={result_image} />
+    {#if $gen_results.length > 0}
+        <PreviewThumbs />
+        <div style="width: {$gen_results[$result_selected].width}px">
+            <ControlBar />
+        </div>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img bind:this={result_image} />
+    {/if}
 </div>
