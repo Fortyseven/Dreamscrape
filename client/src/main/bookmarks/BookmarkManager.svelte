62ff5b62da1311bfc37f62e3dd17912757aa66ef
Fortyseven <fortyseven@network47.org>
2022-10-03 07:11:27 -0400

    Keep bookmark list from growing wildly long, affecting the entire page length

--- client/src/main/bookmarks/BookmarkManager.svelte
+++ client/src/main/bookmarks/BookmarkManager.svelte
@@ -21,57 +21,66 @@
 </div>
 {#if bookmarks}
     {#if view_mode === LIST_MODE}
-        <Table striped>
-            <thead>
-                <tr>
-                    <th>Seed</th>
-                    <th>Prompt</th>
-                    <th>Steps</th>
-                    <th>Scale</th>
-                    <th>Strength</th>
-                </tr>
-            </thead>
-            <tbody>
-                {#each $bookmarks as entry, i}
+        <div id="BookmarkTable">
+            <Table striped hover>
+                <thead>
                     <tr>
-                        <th scope="row">{entry.seed}</th>
-                        <td>{entry.prompt}</td>
-                        <td>{entry.ddim_steps}</td>
-                        <td>{entry.scale}</td>
-                        <td>{entry.strength || "--"}</td>
-                        <td>
-                            <img
-                                src="data:image/jpeg;base64,{entry.thumbnail}"
-                            />
-                        </td>
-                        <td>
-                            <ButtonGroup>
-                                <Button
-                                    on:click={() =>
-                                        promoteBookmarkToSession(entry)}
-                                >
-                                    <Icon
-                                        title="Load Generation Data"
-                                        name="box-arrow-in-up-left"
-                                    />
-                                </Button>
-                                <Button
-                                    on:click={async () => {
-                                        await api.bookmarks.rm(entry.id);
-                                        await api.bookmarks.reload();
-                                    }}
-                                    color="danger"
-                                >
-                                    <Icon
-                                        title="Delete Bookmark"
-                                        name="trash"
-                                    />
-                                </Button>
-                            </ButtonGroup>
-                        </td>
+                        <th>Seed</th>
+                        <th>Prompt</th>
+                        <th>Steps</th>
+                        <th>Scale</th>
+                        <th>Strength</th>
                     </tr>
-                {/each}
-            </tbody>
-        </Table>
+                </thead>
+                <tbody>
+                    {#each $bookmarks as entry, i}
+                        <tr>
+                            <th scope="row">{entry.seed}</th>
+                            <td>{entry.prompt}</td>
+                            <td>{entry.ddim_steps}</td>
+                            <td>{entry.scale}</td>
+                            <td>{entry.strength || "--"}</td>
+                            <td>
+                                <img
+                                    src="data:image/jpeg;base64,{entry.thumbnail}"
+                                />
+                            </td>
+                            <td>
+                                <ButtonGroup>
+                                    <Button
+                                        on:click={() =>
+                                            promoteBookmarkToSession(entry)}
+                                    >
+                                        <Icon
+                                            title="Load Generation Data"
+                                            name="box-arrow-in-up-left"
+                                        />
+                                    </Button>
+                                    <Button
+                                        on:click={async () => {
+                                            await api.bookmarks.rm(entry.id);
+                                            await api.bookmarks.reload();
+                                        }}
+                                        color="danger"
+                                    >
+                                        <Icon
+                                            title="Delete Bookmark"
+                                            name="trash"
+                                        />
+                                    </Button>
+                                </ButtonGroup>
+                            </td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </Table>
+        </div>
     {/if}
 {/if}
+
+<style>
+    #BookmarkTable {
+        height: 50vh;
+        overflow: scroll;
+    }
+</style>
